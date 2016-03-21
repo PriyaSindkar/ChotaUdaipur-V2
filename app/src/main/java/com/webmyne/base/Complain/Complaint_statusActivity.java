13ed2ee6412fbee82a57fbd29b261ba89f3361e8
vaibhavirana <vaibhavi9.rana@yahoo.com>
2016-03-21 20:10:13 +0530

    gfh

---
+++ app/src/main/java/com/webmyne/base/Complain/Complaint_statusActivity.java
@@ -0,0 +1,17 @@
+package com.webmyne.base.Complain;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.webmyne.R;
+
+/**
+ * Created by vaibhavirana on 21-03-2016.
+ */
+public class Complaint_statusActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.complaint_status_main);
+    }
+}
